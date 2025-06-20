f17746429292917895b28793b3ae20b709934b5b
Milos Vasic <i@mvasic.ru>
2025-06-20 21:02:05 +0300

    Init.

---
+++ Catalogizer/settings.gradle.kts
@@ -0,0 +1,19 @@
+dependencyResolutionManagement {
+
+
+    @Suppress("UnstableApiUsage")
+    repositories {
+
+        mavenCentral()
+    }
+}
+
+plugins {
+
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include(":app")
+include(":utils")
+
+rootProject.name = "Catalogizer"
